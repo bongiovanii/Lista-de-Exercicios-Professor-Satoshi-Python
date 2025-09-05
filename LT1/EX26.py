f22c802e27f5efcb14440e8314e3064393599e90
Gustavo Bongiovani <132799669+bongiovanii@users.noreply.github.com>
2025-09-04 21:38:02 -0300

    Create EX26.py

---
+++ LT1/EX26.py
@@ -0,0 +1,8 @@
+#  Receba 2 números inteiros. Verifique e mostre se o maior número é múltiplo do 
+# menor. 
+
+numeros = {
+   'primeiro': int(input("Digite o primeiro numero: ")),
+   'segundo': int(input("Digite o segundo numero: "))
+}
+
