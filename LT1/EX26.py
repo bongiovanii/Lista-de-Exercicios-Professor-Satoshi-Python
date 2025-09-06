dc87605928c9a46a436017f23b692f8a9f586f3f
Gustavo Bongiovani <132799669+bongiovanii@users.noreply.github.com>
2025-09-06 09:02:32 -0300

    Update EX26.py

--- LT1/EX26.py
+++ LT1/EX26.py
@@ -5,4 +5,21 @@ numeros = {
    'primeiro': int(input("Digite o primeiro numero: ")),
    'segundo': int(input("Digite o segundo numero: "))
 }
+print("-"*60)
+if(numeros['primeiro'] > numeros['segundo']):
+   print(f"O numero {numeros['primeiro']} é o maior")
+   if (numeros['primeiro'] % numeros['segundo'] == 0):
+      print(f"O numero {numeros['primeiro']} é divisível pelo {numeros['segundo']}")
+   else:
+      print(f"O numero {numeros['primeiro']} não é divisível pelo {numeros['segundo']}")
+
+print("-"*60)
+
+if(numeros['segundo'] > numeros['primeiro']):
+   print(f"O numero {numeros['segundo']} é o maior")
+   if (numeros['segundo'] % numeros['primeiro'] == 0):
+      print(f"O numero {numeros['segundo']} é divisível pelo {numeros['primeiro']}")
+   else:
+      print(f"O numero {numeros['segundo']} não é divisível pelo {numeros['primeiro']}")
+
 
