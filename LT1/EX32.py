85072803722301e498288b92c43eb893ed369edc
Gustavo Bongiovani <132799669+bongiovanii@users.noreply.github.com>
2025-09-22 22:32:51 -0300

    Create EX32.py

---
+++ LT1/EX32.py
@@ -0,0 +1,7 @@
+# Receba um número inteiro. Calcule e mostre o seu fatorial.
+
+n = int(input("Digite um número: "))
+fatorial = 1
+for i in range(1, n + 1):
+    fatorial *= i
+print(f"O fatorial de {n} é {fatorial}")
